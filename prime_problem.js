4253b043b0b573f6a3392a81421a037a437e58c3
pranaytandel09 <pranaytandel09@gmail.com>
2023-02-12 14:59:30 +0530

    Problem on prime number

---
+++ prime_problem.js
@@ -0,0 +1,15 @@
+// prob on prime number;
+let number= 12;
+let count=0;
+for(let j=1; j<number;j++){
+  if(number%j==0){
+    count++;
+  }
+}
+if(count==2){
+  console.log("Given number is a prime number");
+}
+else {
+    console.log("Given number is not a prime number");
+}
+
